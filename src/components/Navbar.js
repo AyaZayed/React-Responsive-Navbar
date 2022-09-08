1ec48ccfae5750cf08d33ec5d4296ac24745af54
Aya Zayed <ayazayed@std.mans.edu.eg>
2022-09-08 04:00:27 +0200

    desktop view

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,31 @@
+import Logo from "../images/logo-removebg.png";
+
 function Navbar() {
-  return <></>;
+  return (
+    <nav className="navbar">
+      <a href="/" className="nav-logo">
+        <img src={Logo} alt="logo" />
+        <h2>Navbar</h2>
+      </a>
+      <ul className="nav-links">
+        <li>
+          <a href="/">About</a>
+        </li>
+        <li>
+          <a href="/">Docs</a>
+        </li>
+        <li>
+          <a href="/">Contact</a>
+        </li>
+        <li>
+          <button className="login-button">Log in</button>
+        </li>
+        <li>
+          <button className="signup-button">Sign Up</button>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
