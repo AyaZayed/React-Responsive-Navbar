beb4700bc9cf4f30d14494651600544d9fd999b1
Aya Zayed <ayazayed@std.mans.edu.eg>
2022-09-08 03:30:36 +0200

    initial setup

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../css/normalize.css";
+import "../css/App.css";
+import Navbar from "./Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
